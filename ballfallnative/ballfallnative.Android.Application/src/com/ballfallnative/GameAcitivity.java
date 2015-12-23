1af2e1a69b23992f972d2c76906e3bef40953767
lakizoli <lakizoli@yahoo.com>
2015-12-23 23:31:56 +0100

    Működni kezdett a test reklám...

--- ballfallnative/ballfallnative.Android.Application/src/com/ballfallnative/GameAcitivity.java
+++ ballfallnative/ballfallnative.Android.Application/src/com/ballfallnative/GameAcitivity.java
@@ -4,21 +4,24 @@ import android.app.NativeActivity;
 import android.app.ActionBar;
 import android.os.Bundle;
 import android.content.*;
+import android.content.res.*;
 import android.view.*;
 import android.graphics.*;
 import android.graphics.drawable.*;
 import android.widget.*;
 import android.util.*;
-//import com.google.android.gms.ads.*;
+import com.google.android.gms.ads.*;
 
 public class GameAcitivity extends NativeActivity {
 	private LinearLayout _mainLayout;
+
 	private PopupWindow _topLeftOverlay;
 	private PopupWindow _topRightOverlay;
 	private TextView _textTopRight;
 	private TextView _textTopLeft;
 
-	//private AdView _adView;
+	private PopupWindow _adOverlay;
+	private AdView _adView;
 
 	static {
 		System.loadLibrary("ballfallnative");
@@ -57,9 +60,32 @@ public class GameAcitivity extends NativeActivity {
 		_textTopLeft.setTypeface(tf, Typeface.NORMAL);
 
 		//Setup adMob overlay
-		//_adView = new AdView(this);
-		//AdRequest adRequest = new AdRequest.Builder().build();
-        //_adView.loadAd(adRequest);
+		_adView = new AdView(this);
+		_adView.setAdUnitId("ca-app-pub-5492739363747274/1590233145");
+		_adView.setAdSize(AdSize.BANNER);
+
+		AdRequest adRequest = new AdRequest.Builder()
+				.addTestDevice("54F567A97CA221C8AF6DC24725DD98A9")
+				.build();
+        _adView.loadAd(adRequest);
+	}
+
+	@Override
+	protected void onPause() {
+		_adView.pause();
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		_adView.resume();
+	}
+
+	@Override
+	protected void onDestroy() {
+		_adView.destroy();
+		super.onDestroy();
 	}
 
 	public void setTopLeftStyle(final float size, final float red, final float green, final float blue, final float alpha) {
@@ -68,8 +94,11 @@ public class GameAcitivity extends NativeActivity {
 		runOnUiThread(new Runnable() {
 			@Override
 			public void run() {
+				if (_adOverlay == null)
+					_adOverlay = createOverlay (activity, 320, 50, 0, 0, _adView, Gravity.TOP, -5, -5, -5, -5);
+
 				if (_topLeftOverlay == null)
-					_topLeftOverlay = createOverlay (activity, 320, 50, _textTopLeft, Gravity.TOP | Gravity.LEFT);
+					_topLeftOverlay = createOverlay (activity, 320, 50, 0, 50, _textTopLeft, Gravity.TOP | Gravity.LEFT, 5, 0, 5, 0);
 
 				_textTopLeft.setTextSize(TypedValue.COMPLEX_UNIT_DIP, size);
 
@@ -89,7 +118,7 @@ public class GameAcitivity extends NativeActivity {
 			@Override
 			public void run() {
 				if (_topRightOverlay == null)
-					_topRightOverlay = createOverlay (activity, 320, 50, _textTopRight, Gravity.TOP | Gravity.RIGHT);
+					_topRightOverlay = createOverlay (activity, 320, 50, 0, 50, _textTopRight, Gravity.TOP | Gravity.RIGHT, 5, 0, 5, 0);
 
 				_textTopRight.setTextSize(TypedValue.COMPLEX_UNIT_DIP, size);
 
@@ -120,7 +149,7 @@ public class GameAcitivity extends NativeActivity {
 		});
 	}
 
-	private PopupWindow createOverlay (Context context, int width, int height, TextView overlayView, int gravity) {
+	private PopupWindow createOverlay (Context context, int width, int height, int offsetX, int offsetY, View overlayView, int gravity, int padLeft, int padTop, int padRight, int padBottom) {
 		PopupWindow popUp = new PopupWindow(context);
 
 		popUp.setWidth(width);
@@ -130,7 +159,7 @@ public class GameAcitivity extends NativeActivity {
 
 		LinearLayout layout = new LinearLayout(context);
 		layout.setOrientation(LinearLayout.VERTICAL);
-		layout.setPadding(5, 0, 5, 0);
+		layout.setPadding(padLeft, padTop, padRight, padBottom);
 
 		ViewGroup.MarginLayoutParams params = new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 		params.setMargins(0, 0, 0, 0);
@@ -140,9 +169,21 @@ public class GameAcitivity extends NativeActivity {
 
 		popUp.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-		popUp.showAtLocation(_mainLayout, gravity, 0, 0);
+		popUp.showAtLocation(_mainLayout, gravity, convertDpToPixel (offsetX), convertDpToPixel (offsetY));
 		popUp.update();
 
 		return popUp;
+	}
+
+	private int convertPixelsToDp(float px) {
+		DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+		float dp = px / (metrics.densityDpi / (float)DisplayMetrics.DENSITY_DEFAULT);
+		return Math.round(dp);
+	}
+
+	private int convertDpToPixel(float dp) {
+		DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+		float px = dp * (metrics.densityDpi / (float)DisplayMetrics.DENSITY_DEFAULT);
+		return Math.round(px);
 	}
 }
